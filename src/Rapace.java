4300bce6e801181bb0cd2de0adcba16318149f10
AleksandarBrkovic <aleksandarbrkovic47@gmail.com>
2022-02-19 20:31:28 +0100

    vediamo se mo salva

--- src/Rapace.java
+++ src/Rapace.java
@@ -2,7 +2,17 @@
 public class Rapace extends Uccelli {
 
 	public Rapace(int vel, int f, int vit, int ene, int att, int dif, int da, int alt, int lung, int larg, int pe) {
-		super(vel, f, vit, ene, att, dif, da, alt, lung, larg, pe);
+		super(vel= (int) Math.random()*10,
+				f, 
+				vit, 
+				ene, 
+				att, 
+				dif, 
+				da, 
+				alt, 
+				lung, 
+				larg, 
+				pe);
 		// TODO Auto-generated constructor stub
 	}
 
